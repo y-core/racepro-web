00df02f919f8b23b307c85f5f604390507084277
Gustav Bouwer <gustavbouwer@Gustavs-MacBook-Pro.local>
2024-04-05 07:44:10 +0200

    Update schema

--- src/schema/20240327.sql.ts
+++ src/schema/20240327.sql.ts
@@ -22,6 +22,32 @@ export const event: SqlLite = {
     { name: "event_series", type: "TEXT", notNull: true },
     { name: "created_date", type: "INTEGER", default: "CURRENT_TIMESTAMP"},
     { name: "event_created_by_user_id", type: "INTEGER", notNull: true },
+    { name: "event_primary_user", type: "INTEGER", notNull: true },
+    { name: "event_address", type: "TEXT", notNull: true },
+    { name: "event_address_id", type: "INTEGER", notNull: true },
+    { name: "event_date", type: "INTEGER"},
+    { name: "event_start_time", type: "INTEGER"},
+    { name: "event_end_time", type: "INTEGER"},
+    { name: "event_distance", type: "INTEGER",notNull: true},
+    { name: "event_elevation_gain", type: "INTEGER"},
+    { name: "event_total_tickets", type: "INTEGER"},
+    { name: "event_type_tla", type: "TEXT",notNull: true},
+    { name: "event_category_tla", type: "TEXT",notNull: true},
+    { name: "event_subcategory_tla", type: "TEXT",notNull: true},
+    { name: "event_organiser_id", type: "INTEGER",notNull: true},
+    { name: "event_town", type: "TEXT",notNull: true},
+    { name: "event_city", type: "TEXT",notNull: true},
+    { name: "event_country", type: "TEXT",notNull: true},
+    { name: "event_fees", type: "INTEGER"},
+    { name: "event_last_modified_date", type: "INTEGER",notNull: true},
+    { name: "event_entry_details", type: "TEXT"},
+    // What is boolean type? Can also do a race qualifier tla multiselect instead of boolean for just comrades
+    { name: "event_comrades_qualifier", type: "INT"},
+    { name: "event_race_day_information", type: "TEXT"},
+    // { name: "event_coordinates", type: "TEXT"},
+    // Booleans 1s and 0s or text
+  
+    { name: "event_verified", type: "INT",notNull: true, default:"0"},
 
   ],
   indexes: [
